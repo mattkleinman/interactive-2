8d81ec0efeed1c16975ab990c693412455b4e962
m k <mattkleinman@Matts-MacBook-Pro.local>
2017-09-25 08:09:58 -0700

    a.ksd

--- p1/assets/script.js
+++ p1/assets/script.js
@@ -1,3 +1,22 @@
-$(function(){
+/*$(function(){
     $("#heart").click(function(){$(this).toggleClass("active")})
+})
+*/
+
+$(function(){
+	var i = .25;
+  $("#heart").click(function(){
+  	i++;
+  	$(this).css({
+    	transform: `scale(${i},${i})`
+    });
+  })
+})
+
+$(function(){
+	$("#heart").click(function(){
+  				$("#yes")[0].pause();
+  				$("#yes")[0].currentTime = 0;
+  				$("#yes")[0].play();
+  				});
 })
