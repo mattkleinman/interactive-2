f9605d3eb6a3500ea2b954ad9bd1e04326b1f9d9
m k <mattkleinman@Matts-MacBook-Pro-2.local>
2017-09-27 12:19:49 -0700

    whew

--- p1/assets/script.js
+++ p1/assets/script.js
@@ -2,9 +2,18 @@
     $("#heart").click(function(){$(this).toggleClass("active")})
 })
 */
-$(function(){
-    $("#whiteheart").click(function(){$(this).toggleClass("active")})
-})
+$(document).ready(function(){
+
+
+
+	/*$(function(){
+		$("#heart").click(function(){
+  				
+  				$("#wig")[0].play();
+  				});
+		});*/
+
+});
 
 $(function(){
 	var i = .25;
@@ -16,10 +25,43 @@ $(function(){
   })
 })
 
-$(function(){
-	$("#heart").click(function(){
-  				$("#yes")[0].pause();
-  				$("#yes")[0].currentTime = 0;
-  				$("#yes")[0].play();
-  				});
-})
+var sounds = [];
+
+// Return the full list of URLs in random order
+function getSounds() {
+    return [
+        // so tinny...
+        'assets/yes.mp3',
+        'assets/wow.mp3',
+        'assets/legendary.mp3',
+        'assets/wig.mp3',
+        'assets/iconic.mp3',
+        'assets/so hot.mp3',
+        'assets/im shaking.mp3',
+        'assets/cry.mp3',
+        'assets/voices.mp3',
+        'assets/ohmygod.mp3',
+
+    ].sort(function () {
+        // http://stackoverflow.com/a/18650169/283078
+        return (.5 - Math.random());
+    });
+}
+
+// Play the next sound
+function playSound() {
+    if (!sounds.length) {
+        // Get the list of sounds (random order)
+        sounds = getSounds();
+    }
+
+    // Pops a URL every time, ensuring all are played exactly once
+    $("#item").html(
+        '<embed src="' + sounds.pop() +
+        '" hidden="true" autostart="true" />'
+    );
+
+    // Once all the URLs have been popped, the array is repopulated
+}
+
+$('#heart').click(playSound);
